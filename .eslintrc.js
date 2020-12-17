79b222b0d37d1db354b614a457868fb83f05da92
shibayu36 <shibayu36@gmail.com>
2020-12-17 09:32:28 +0900

    一回整理

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
   },
   overrides: [
     {
-      files: ["src/**/*.ts"],
+      files: ["**/*.ts"],
       extends: [
         "plugin:@typescript-eslint/eslint-recommended",
         "plugin:@typescript-eslint/recommended",
