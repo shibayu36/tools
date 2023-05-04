1b25649e78bc0b4cc414020b71d3f7ccb03931fe
shibayu36 <shibayu36@gmail.com>
2023-05-04 15:47:56 +0900

    Revert "Improve the prompt for summarization so that ChatGPT can ignore unnecessary quote"
    This reverts commit 78be83f4668bac250905171833787f2c9b19fc14.

--- summarize-text.py
+++ summarize-text.py
@@ -19,11 +19,9 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.prompts import PromptTemplate
 
 verbose = os.environ.get("VERBOSE") == "1"
-prompt_template = """Write a concise summary of the following text delimited by triple backticks.
-If you find a section that appears to be a quote that has nothing to do with the main point of the story, please do not use that section in your summary.
-Your output must be written in Japanese
+prompt_template = """Write a concise summary of the following text in Japanese:
 
-```{text}```
+{text}
 """
 
 text = sys.stdin.read()
